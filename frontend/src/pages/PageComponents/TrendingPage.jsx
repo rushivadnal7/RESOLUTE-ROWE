0e15227fff0ffea04494ca01a33a993d0a5f5c50
rushikeshtreb <rushikeshvadnal7@gmail.com>
2025-01-26 19:30:54 +0530

    changes

--- frontend/src/pages/PageComponents/TrendingPage.jsx
+++ frontend/src/pages/PageComponents/TrendingPage.jsx
@@ -9,8 +9,9 @@ const TrendingPage = () => {
   // Scroll to the left
   const scrollLeft = () => {
     if (scrollContainerRef.current) {
+      const cardWidth = scrollContainerRef.current.firstChild.offsetWidth; // Get the width of a single card
       scrollContainerRef.current.scrollBy({
-        left: -300, // Adjust scroll distance as needed
+        left: -cardWidth, // Scroll by one card's width
         behavior: "smooth",
       });
     }
@@ -19,32 +20,34 @@ const TrendingPage = () => {
   // Scroll to the right
   const scrollRight = () => {
     if (scrollContainerRef.current) {
+      const cardWidth = scrollContainerRef.current.firstChild.offsetWidth; // Get the width of a single card
       scrollContainerRef.current.scrollBy({
-        left: 300, // Adjust scroll distance as needed
+        left: cardWidth, // Scroll by one card's width
         behavior: "smooth",
       });
     }
   };
 
-  // data-scroll data-scroll-section data-scroll-speed="-0.1" 
+  let dummy = [1,2,3,4,5]
+
   return (
     <section className="trending-page">
-      {/* <div className="arrow-controls"> */}
-        <button className="arrow left-arrow" onClick={scrollLeft}>
-          &#8249;
-        </button>
+      <button className="arrow left-arrow" onClick={scrollLeft}>
+        &#8249;
+      </button>
 
-        <h2 className="title-trending">TRENDING</h2>
-        <div className="scrolling-container" ref={scrollContainerRef}>
-          {allProducts.reverse().map((val, id) => (
-            <ProductCard key={id} data={val} />
-          ))}
-        </div>
+      <h2 className="title-trending">TRENDING</h2>
+      <div className="scrolling-container" ref={scrollContainerRef}>
+        {allProducts.length === 0 ? dummy.map(()=> {
+          return <ProductCard />
+        }) :  allProducts.reverse().map((val, id) => (
+          <ProductCard key={id} data={val} />
+        ))}
+      </div>
 
-        <button className="arrow right-arrow" onClick={scrollRight}>
-          &#8250; 
-        </button>
-      {/* </div> */}
+      <button className="arrow right-arrow" onClick={scrollRight}>
+        &#8250;
+      </button>
     </section>
   );
 };
