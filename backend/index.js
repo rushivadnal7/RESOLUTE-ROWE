f36a96ef4e56c67cc39e7f6f193ba36ff263fa97
rushikeshtreb <rushikeshvadnal7@gmail.com>
2025-02-20 15:01:36 +0530

    debugging

--- backend/index.js
+++ backend/index.js
@@ -52,13 +52,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
